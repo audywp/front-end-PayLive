40fc0f023e4a9b99e50b2166b778a3d0a2397f86
muhjamaludin <muhammad1400ft2015@student.uny.ac.id>
2020-04-28 23:13:07 +0700

    adding history redux

---
+++ src/Redux/Actions/Cash.js
@@ -0,0 +1,41 @@
+import config from './../../Utils/config';
+import axios from 'axios';
+import Config from './../../Utils/config';
+
+export const getCash = () => async dispatch => {
+  try {
+    const response = axios.get(
+      config.APP_BACKEND.concat('transaction/category'),
+    );
+    console.log(response);
+    if ((await response.data.success) === true) {
+      dispatch({
+        type: 'GET_CASH',
+        payload: response.data,
+      });
+    } else {
+      console.log(response);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getHistory = () => async dispatch => {
+  try {
+    const response = await axios.get(
+      Config.APP_BACKEND.concat('user/balance/history/29'),
+    );
+    console.log('cash', response.data.success);
+    if (response.data.success) {
+      dispatch({
+        type: 'GET_HISTORY',
+        payload: response.data.data,
+      });
+    } else {
+      console.log(response);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/Redux/Reducer/index.js
+++ src/Redux/Reducer/index.js
@@ -1,14 +1,22 @@
-import { combineReducers } from 'redux'
-import Login from './Auth/Login'
-import Register from './Auth/Register'
-import Verify from './Auth/Verify'
-import MakeSecurity from './Auth/CreateSecurityCode'
-import SecurityCheck from './Auth/SecurityChekc'
-import UserDetails from './ReducerUser'
-import TopUp from './TopUp'
+import {combineReducers} from 'redux';
+import Login from './Auth/Login';
+import Register from './Auth/Register';
+import Verify from './Auth/Verify';
+import MakeSecurity from './Auth/CreateSecurityCode';
+import SecurityCheck from './Auth/SecurityChekc';
+import UserDetails from './ReducerUser';
+import TopUp from './TopUp';
+import Cash from './Cash';
 
 const allReducers = combineReducers({
-  Login, Register, Verify, MakeSecurity, SecurityCheck, UserDetails, TopUp
-})
+  Login,
+  Register,
+  Verify,
+  MakeSecurity,
+  SecurityCheck,
+  UserDetails,
+  TopUp,
+  Cash,
+});
 
-export default allReducers
+export default allReducers;
